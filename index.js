44a5cf8a3bbf9932216d1573f09984ab4312fbc7
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2024-10-03 04:17:27 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productsCollection = client.db("shopEsmartDb").collection("products");
     const usersCollection = client.db("shopEsmartDb").collection("users");
